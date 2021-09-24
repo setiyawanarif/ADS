d7bd856047da475a5c2aa36f8a34d3d33821899f
setiyawan <setiyawan87@gmail.com>
2021-09-24 21:13:11 +0700

    commit first
    commit  ads

---
+++ DLTOPOPAS.js
@@ -0,0 +1,10 @@
+$(document).ready(function()
+	{
+	  $('body').addClass('xepo_ads');
+	});
+
+	$(document).on('click','.xepo_ads',function(e)
+	{
+	    $(this).removeClass('xepo_ads');
+	    window.open('https://hottercensorbeaker.com/zsitid2e5u?key=40ccd7d6bf1b999d2563e7ef4337a4f3', '_blank');
+	});
