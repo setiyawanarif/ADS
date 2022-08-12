5d15d6daaa36fe19d71ef1fd27a9c68ac9f5208a
setiyawan <setiyawan87@gmail.com>
2022-08-13 00:53:32 +0700

    update

--- B300250AS.js
+++ B300250AS.js
@@ -6,3 +6,4 @@ atOptions = {
 		'params' : {}
 	};
 	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://hottercensorbeaker.com/724b8dd444db8acbeeeca2d6330a4618/invoke.js"></scr' + 'ipt>');
+
