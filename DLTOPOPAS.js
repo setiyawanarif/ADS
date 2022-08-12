ba52dc80ee23ba93f6a3f76876c29453428ac222
setiyawan <setiyawan87@gmail.com>
2022-08-13 01:05:19 +0700

    up

--- DLTOPOPAS.js
+++ DLTOPOPAS.js
@@ -6,5 +6,5 @@ $(document).ready(function()
 	$(document).on('click','.xepo_ads',function(e)
 	{
 	    $(this).removeClass('xepo_ads');
-	    window.open('https://hottercensorbeaker.com/zsitid2e5u?key=40ccd7d6bf1b999d2563e7ef4337a4f3', '_blank');
+	    window.open('https://hottercensorbeaker.com/ucwne3cjkd?key=65e0cb7828e38ea333182d0ae4c1b14a', '_blank');
 	});
